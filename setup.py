3ccf8d77a87c58e3cb9fb6e1c07825df87c7deb3
karan <karankumar20080eee@gmail.com>
2026-06-09 22:00:09 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+name="mlprojects",
+version="0.0.1",
+author="karan",
+packages=find_packages(),
+install_requires=["pandas","numpy","seaborn"],    
+    
+    
+    
+    
+)
